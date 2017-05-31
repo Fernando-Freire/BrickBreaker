290578a36eb18806d6b630c92c78df7b30acc1e4
Gustavo Covas <gtcovas@gmail.com>
2017-05-31 09:51:33 -0300

    Add movement to platform

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+	public int speed = 5;
+
+	void FixedUpdate() {
+		float v = Input.GetAxisRaw ("Horizontal");
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (v, 0) * speed;
+	}
+}
